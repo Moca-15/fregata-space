a82454583862dc632371ef3a42a9303151f3a389
Moca-15 <ritacasablancas@gmail.com>
2025-01-20 12:37:45 +0100

    Index.js per importar varios components de cop

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+// index per poder importar diversos components a la vegada
+
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as About } from './About';
+export { default as Solutions } from './Solutions';
+export { default as ContactForm } from './ContactForm';
+export { default as Features } from './Features';
+export { default as Hero } from './Hero'
+export { default as Industries } from './Industries'
+export { default as LanguageSwitcher } from './LanguageSwitcher'
+export { default as Process } from './Process'
+export { default as Testimonials } from './Testimonials'
